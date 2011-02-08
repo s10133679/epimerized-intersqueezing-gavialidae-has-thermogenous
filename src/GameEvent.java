edbb44ca93dbd89712f54e976a30c40824e2cd63
aspiring.arsonist@gmail.com <aspiring.arsonist@gmail.com@38fbf40a-4406-2c4f-7d2f-54aa74e2a72e>
2011-02-08 04:52:02 +0000

    Just adding the empty classes

---
+++ src/GameEvent.java
@@ -0,0 +1,11 @@
+import java.util.EventObject;
+
+
+public class GameEvent extends EventObject {
+
+	public GameEvent(Object source) {
+		super(source);
+		// TODO Auto-generated constructor stub
+	}
+
+}
