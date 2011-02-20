b887fb5fee8a6b26b3081c81d197d6237f15fd19
josh.delrio@gmail.com <josh.delrio@gmail.com@38fbf40a-4406-2c4f-7d2f-54aa74e2a72e>
2011-02-20 21:29:17 +0000

    updated 20/02

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,17 @@
+import java.util.ArrayList;
+
 
 public abstract class Game {
-
+	ArrayList<GameListener> GameList;
+	public Game(){
+		GameList = new ArrayList<GameListener>();
+	}
+	public void addListener(GameListener g){
+		GameList.add(g);
+	}
+	public void notify(GameEvent e){
+		for(GameListener listener:GameList){
+			listener.onEvent(e);
+		}
+	}
 }
