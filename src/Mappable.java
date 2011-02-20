b9e3c899cdf698999ffabcc19903e5af78480b9c
aspiring.arsonist <aspiring.arsonist@gmail.com>
2011-02-20 21:24:18 +0000

    Added private variables "x" and "y", created setters and getters, also made a toString

--- src/Mappable.java
+++ src/Mappable.java
@@ -1,4 +1,30 @@
 
 public abstract class Mappable {
+	private int x;
+	private int y;
+	
+	public Mappable(int x ,int y){
+		this.x = x;
+		this.y = y;
+	}
 
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	@Override
+	public String toString(){
+		return (x + "," + y);
+	}
 }
