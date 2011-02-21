8ed3097935616523bb17935a808c04b2fd51c71c
aspiring.arsonist@gmail.com <aspiring.arsonist@gmail.com@38fbf40a-4406-2c4f-7d2f-54aa74e2a72e>
2011-02-21 06:56:49 +0000

    Recoded using constants, Also refactored some code

--- src/Map.java
+++ src/Map.java
@@ -1,17 +1,17 @@
 import java.util.Hashtable;
 
-
 public class Map {
-	private int map[][];
+	private OnMap map[][];
 	private Hashtable<String,Mappable> mappables;
 	
+
 	public Map(int Xsize, int Ysize){
-		map = new int[Xsize][Ysize];
+		map = new OnMap[Xsize][Ysize];
 	}
 	
 	public boolean addMappable(Mappable m){
 		if (isEmpty(m.getX(),m.getY())){
-			map[m.getX()][m.getY()] = 1;
+			map[m.getX()][m.getY()] = OnMap.MAPPABLE;
 			mappables.put(m.toString(), m);
 			return true;
 		}
@@ -21,23 +21,20 @@ public class Map {
 	
 	public boolean addWall(int x, int y){
 		if(isEmpty(x,y)){
-			map[x][y] = -1;
+			map[x][y] = OnMap.WALL;
 			return true;
 		}
 		return false;
 	}
+	
 	public boolean isEmpty(int x, int y){
-		if (map[x][y] == 0){
-			return true;
-		}
-		return false;
+		return map[x][y] == OnMap.EMPTY;
 	}
+	
 	public boolean isWall(int x, int y){
-		if( map[x][y] == -1){
-			return true;
-		}
-		return false;
+		return map[x][y] == OnMap.WALL;
 	}
+	
 	public boolean removeMappable(int x, int y){
 		//Checks to see if anything is in the spot
 		if(isEmpty(x,y) && !isWall(x,y)){
