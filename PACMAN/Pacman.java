4b21bbfca8b0fa7c8df7077a005a626ec175a18c
Argonanth <Argonanth@gmail.com>
2011-02-27 15:53:25 +0000

    added all the new classes for the pacman game -Mappable was supposed to be a GameListener (fixed)

---
+++ PACMAN/Pacman.java
@@ -0,0 +1,38 @@
+
+public class Pacman extends Player {
+
+	public Pacman(int x, int y) {
+		super(x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void moveDown() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void moveLeft() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void moveRight() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void moveUp() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void onEvent(GameEvent e) {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ PACMAN/World.java
@@ -0,0 +1,14 @@
+
+public class World extends Map{
+
+	public World(int Xsize, int Ysize) {
+		super(Xsize, Ysize);
+		// TODO Auto-generated constructor stub
+	}
+
+	public World(String fileName) {
+		super(fileName);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
