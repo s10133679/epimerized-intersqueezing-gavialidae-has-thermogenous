2c8fad9768fbf44862f9838b88d9a96b6603fb7d
aspiring.arsonist <aspiring.arsonist@gmail.com>
2011-02-28 11:06:13 +0000

    added image

--- src/Mappable.java
+++ src/Mappable.java
@@ -1,8 +1,12 @@
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
 
 public abstract class Mappable {
 	private int x;
 	private int y;
-	
+	private Image image;
 	public Mappable(int x ,int y){
 		this.x = x;
 		this.y = y;
@@ -28,6 +32,17 @@ public abstract class Mappable {
 	@Override
 	public String toString(){
 		return (x + "," + y);
+	}
+
+	public void setImage(Image image){
+		this.image = image;
+	}
+	
+	public void setImage(String filepath){
+		this.image =  new ImageIcon(filepath).getImage();
+	}
+	public Image getImage() {
+		return image;
 	}
 
 }
