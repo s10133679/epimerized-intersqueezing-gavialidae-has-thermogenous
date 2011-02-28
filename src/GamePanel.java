11d9bfc642a046abf98e399408657f6c5464c206
aspiring.arsonist@gmail.com <aspiring.arsonist@gmail.com@38fbf40a-4406-2c4f-7d2f-54aa74e2a72e>
2011-02-28 10:12:23 +0000

    TODO:finish this up, takes care of most of the painting for us.

---
+++ src/GamePanel.java
@@ -0,0 +1,13 @@
+import javax.swing.JPanel;
+
+
+public abstract class GamePanel extends JPanel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2458261363554188485L;
+
+	
+	public abstract void newGame();
+}
