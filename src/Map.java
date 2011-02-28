a72948f38f115578ae975d1820b24ec3beaa36d9
aspiring.arsonist@gmail.com <aspiring.arsonist@gmail.com@38fbf40a-4406-2c4f-7d2f-54aa74e2a72e>
2011-02-28 08:26:45 +0000

    Added comments, A lot of rework, Resolved logic error

--- src/Map.java
+++ src/Map.java
@@ -1,14 +1,21 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Hashtable;
 
 public abstract class Map {
 	protected Game game;
 	protected OnMap map[][];
-	protected Hashtable<String,Mappable> mappables;
+	protected Hashtable<String,ArrayList<Mappable>> mappables;
 	protected int xSize, ySize;
 	
+	/*
+	 * Constructs an empty map of size Xsize by Ysize.
+	 * @param Xsize the number of X tiles on the map.
+	 * @param Ysize the number of Y tiles on the map.
+	 * @param Game i unno bout this one, alex wanna add something here?
+	 */
 	public Map(int Xsize, int Ysize, Game game){
 		this.xSize = Xsize;
 		this.ySize = Ysize;
@@ -18,11 +25,19 @@ public abstract class Map {
 				map[x][y] = OnMap.EMPTY;
 			}
 		}
-		this.mappables = new Hashtable<String,Mappable>();
+		this.mappables = new Hashtable<String,ArrayList<Mappable>>();
+		this.game = game;
 	}
 	
+	/*
+	 * Constructs an empty map from a file.
+	 * <p>
+	 * See wiki page for full details on thie method
+	 * @param filename name of file containing map layout
+	 * @param Game i unno bout this one, alex wanna add something here?
+	 */
 	public Map(String fileName, Game game) {
-		this.mappables = new Hashtable<String,Mappable>();
+		this.mappables = new Hashtable<String,ArrayList<Mappable>>();
 		try{
 			BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
 			String s = in.readLine();
@@ -30,7 +45,7 @@ public abstract class Map {
 			this.xSize = sToInt(nums[0]);
 			this.ySize = sToInt(nums[1]);
 			this.map = new OnMap[xSize][ySize];
-			
+			this.game = game;
 			int y = 0;
 			while( (s = in.readLine()) != null){
 				nums = s.split(",");
@@ -74,16 +89,36 @@ public abstract class Map {
 		return i;
 	}
 
+	/*
+	 * Adds a Mappable Object to the Map.
+	 * <p>
+	 * Adds a Mappable to an empty tile on the map and then marks that tile as MAPPABLE. 
+	 * If there already is a Mappable in the position, it is still added and all current
+	 * Mappables at that tile will be put into an ArrayList.
+	 * @param m The Mappable that needs to be added.
+	 * @return true if the add was successful, false otherwise.
+	 */
 	public boolean addMappable(Mappable m){
 		
-		if (isEmpty(m.getX(),m.getY())){
+		if (!isWall(m.getX(),m.getY())){
 			map[m.getX()][m.getY()] = OnMap.MAPPABLE;
-			mappables.put(m.toString(), m);
+			ArrayList<Mappable> onDmap = mappables.get((m.getX() + "," + m.getY()));
+			if( onDmap == null){
+				onDmap = new ArrayList<Mappable>();
+				mappables.put(m.toString(), onDmap);
+			}
+			onDmap.add(m);
 			return true;
 		}
 		return false;
 	}
 	
+	/*
+	 * Adds a wall in the specified tile.
+	 * @param x the X position of the desired tile
+	 * @param y the Y position of the desired tile
+	 * @return true if add was successful, false if it failed.
+	 */
 	public boolean addWall(int x, int y){
 		
 		if(isEmpty(x,y)){
@@ -93,29 +128,78 @@ public abstract class Map {
 		return false;
 	}
 	
+	/*
+	 * Checks to see if tile is empty.
+	 * @param x the X position of the desired tile
+	 * @param y the Y position of the desired tile
+	 * @return true if the tile is empty, false otherwise or if x and y are out of bounds
+	 */
 	public boolean isEmpty(int x, int y){
 		return ((!isOutOfBounds(x,y))&&(map[x][y] == OnMap.EMPTY));
 	}
 	
+	/*
+	 * Checks to see if tile is a wall.
+	 * @param x the X position of the desired tile
+	 * @param y the Y position of the desired tile
+	 * @return true if the tile is a wall, false otherwise or if x and y are out of bounds
+	 */
 	public boolean isWall(int x, int y){
 		return ((!isOutOfBounds(x,y)) && (map[x][y] == OnMap.WALL));
 	}
 	
+	/*
+	 * Checks to see if desired tile is within the map.
+	 * @param x the X position of the desired tile
+	 * @param y the Y position of the desired tile
+	 * @return true if such a tile exits, false otherwise
+	 */
 	public boolean isOutOfBounds(int x, int y){
 		return (x >= this.xSize || y >= this.ySize || x < 0 || y < 0);
 	}
 	
-	public boolean removeMappable(int x, int y){
+	/*
+	 * Attempts to remove a Mappable from the Map
+	 * @param x the X position of the desired tile
+	 * @param y the Y position of the desired tile
+	 * @param pos position of the Mappable in the tile's ArrayList
+	 * @return true if a Mappable was removed, false otherwise
+	 */
+	public boolean removeMappable(int x, int y, int pos){
 
 		//Checks to see if anything is in the spot
 		if(isEmpty(x,y) || isWall(x,y)){
 			return false;
 		}
-		mappables.remove((x + "," + y));
-		map[x][y] = OnMap.EMPTY;
+		ArrayList<Mappable> onDmap = mappables.get((x + "," + y));
+		//ArrayList will not be null otherwise it would not have made this far in the code.
+		onDmap.remove(pos);
+		if(onDmap.size() == 0){
+			/*
+			 * remove comment if you want to remove the empty list from the HashTable
+			 *mappables.remove((x + "," + y));
+			 */
+			map[x][y] = OnMap.EMPTY;
+		}
 		return true;
 	}
 	
+	/*
+	 * Attempts to remove a Mappable from the Map
+	 * @param x the X position of the desired tile
+	 * @param y the Y position of the desired tile
+	 * @return true if a Mappable was removed, false otherwise
+	 */
+	public boolean removeMappable(int x, int y){
+		return removeMappable(x,y,0);
+	}
+	
+	/*
+	 * Attempts to remove a wall
+	 * @param x the X position of the desired tile
+	 * @param y the Y position of the desired tile
+	 * @return true if wall was removed, false otherwise
+	 */
 	public boolean removeWall(int x, int y){
 		//Checks to see if there is a wall
 		if(!isWall(x,y)){
@@ -125,14 +209,41 @@ public abstract class Map {
 		return true;
 	}
 	
-	public Mappable getMappable(int x, int y) {
-		if (isEmpty(x,y) && isWall(x,y)) return null;
-		return mappables.get(x + "," + y);
+	/*
+	 * Attempts to retrieve a Mappable from a desired tile
+	 * @param x the X position of the desired tile
+	 * @param y the Y position of the desired tile
+	 * @return The desired Mappable or null if Mappable is present at that location
+	 */
+	public Mappable getMappable(int x, int y){
+		return getMappable(x,y,0);
 	}
+	
+	/*
+	 * Attempts to retrieve a Mappable from a desired tile
+	 * @param x the X position of the desired tile
+	 * @param y the Y position of the desired tile
+	 * @param pos the position of the Mappable in the ArrayList at that position
+	 * @return The desired Mappable or null if Mappable is present at that location
+	 */
+	public Mappable getMappable(int x, int y, int pos) {
+		if (isEmpty(x,y) && isWall(x,y)) return null;
+		return ((ArrayList<Mappable>)mappables.get(x + "," + y)).get(pos);
+	}
+	
+	/*
+	 * Retrieves Map's current X size
+	 * @return Map's Xsize variable
+	 */
 	
 	public int getX() {
 		return xSize;
 	}
+	
+	/*
+	 * Retrieves Map's current Y size
+	 * @return Map's Ysize variable
+	 */
 	
 	public int getY() {
 		return ySize;
