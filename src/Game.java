e1e1ff89449efa1c12646898d3c7372655a8c41a
aspiring.arsonist@gmail.com <aspiring.arsonist@gmail.com@38fbf40a-4406-2c4f-7d2f-54aa74e2a72e>
2011-02-28 15:50:06 +0000

    Added proper commenting

--- src/Game.java
+++ src/Game.java
@@ -4,18 +4,36 @@ import java.util.ArrayList;
 public abstract class Game {
 	protected ArrayList<GameListener> GameList;
 	protected Map map;
+	/**
+	 * Creates a new game
+	 */
 	public Game(){
 		GameList = new ArrayList<GameListener>();
 	}
+	/**
+	 * @return Current Map of game
+	 */
 	public Map getMap() {
 		return map;
 	}
+	/**
+	 * Allows you to change current map of game
+	 * @param map new map that you want game to use
+	 */
 	public void setMap(Map map) {
 		this.map = map;
 	}
+	/**
+	 * Lets you add listeners for events
+	 * @param g Listener to be added
+	 */
 	public void addListener(GameListener g){
 		GameList.add(g);
 	}
+	/**
+	 * Notifies all listeners of Event
+	 * @param e Event that needs to be sent to listeners
+	 */
 	public void notify(GameEvent e){
 		for(GameListener listener:GameList){
 			listener.onEvent(e);
