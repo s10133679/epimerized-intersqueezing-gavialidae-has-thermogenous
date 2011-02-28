067c62a75b6c8b358dbf107aeb031d6604e85a4c
Argonanth@gmail.com <Argonanth@gmail.com@38fbf40a-4406-2c4f-7d2f-54aa74e2a72e>
2011-02-28 01:56:10 +0000

    Big changes... redesigning how the game is made... wasnt working good the other way at all

--- MOUSELAND/Level.java
+++ MOUSELAND/Level.java
@@ -1,13 +1,13 @@
 
-public class Level extends Map{
+public class Level extends Map {
 
-	public Level(int Xsize, int Ysize) {
-		super(Xsize, Ysize);
+	public Level(int Xsize, int Ysize, MouseLand game) {
+		super(Xsize, Ysize, game);
 		// TODO Auto-generated constructor stub
 	}
 	
-	public Level(String fileName) {
-		super(fileName);
+	public Level(String fileName, MouseLand game) {
+		super(fileName, game);
 		// TODO Auto-generated constructor stub
 	}
 
