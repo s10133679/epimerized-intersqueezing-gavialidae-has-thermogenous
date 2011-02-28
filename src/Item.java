a4c2621886c7c57652af51cad4920db5f8103688
aspiring.arsonist@gmail.com <aspiring.arsonist@gmail.com@38fbf40a-4406-2c4f-7d2f-54aa74e2a72e>
2011-02-28 15:56:27 +0000

    Added proper commenting

--- src/Item.java
+++ src/Item.java
@@ -1,9 +1,18 @@
 
 public abstract class Item extends Mappable {
 
+	/**
+	 * Creates an Item at position x,y
+	 * @param x X coordinate of Item
+	 * @param y Y coordinate of Item
+	 */
 	public Item(int x, int y) {
 		super(x, y);
 		// TODO Auto-generated constructor stub
 	}
+	/**
+	 * Abstract method which forces items to have some effect
+	 * @param obj an object the item affects
+	 */
 	public abstract void action(Object obj);
 }
