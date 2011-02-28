0b6068646e8372e7378a531151ed020c381c293f
aspiring.arsonist <aspiring.arsonist@gmail.com>
2011-02-28 17:07:21 +0000

    empty more files needed

---
+++ PACMAN/Ghost.java
@@ -0,0 +1,21 @@
+
+public class Ghost extends Player {
+
+	public Ghost(int x, int y) {
+		super(x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void spawn() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onEvent(GameEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
