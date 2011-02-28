d7c33d934db56bed43d8041e50640b4e9630dffe
aspiring.arsonist <aspiring.arsonist@gmail.com>
2011-02-28 15:54:43 +0000

    Added proper commenting

--- src/GameEvent.java
+++ src/GameEvent.java
@@ -2,17 +2,23 @@ import java.util.EventObject;
 
 
 public class GameEvent extends EventObject {
-	/**
-	 * 
-	 */
+	
 	protected static final long serialVersionUID = 1L;
 	protected Object m;
+	/**
+	 * Creates a GameEvent
+	 * @param source object that is creating the Event
+	 * @param m object that the event affects
+	 */
 	public GameEvent(Object source, Object m) {
 		super(source);
 		this.m = m;
 		
 		// TODO Auto-generated constructor stub
 	}
+	/**
+	 * @return returns affected Object
+	 */
 	public Object getGameValue(){
 		return m;
 	}
