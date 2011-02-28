20864e45c203b44f4a53ed16817f03e3cea27296
aspiring.arsonist@gmail.com <aspiring.arsonist@gmail.com@38fbf40a-4406-2c4f-7d2f-54aa74e2a72e>
2011-02-28 17:07:21 +0000

    empty more files needed

---
+++ PACMAN/BigPillItem.java
@@ -0,0 +1,21 @@
+
+public class BigPillItem extends Item {
+
+	public BigPillItem(int x, int y) {
+		super(x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void action(Object obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onEvent(GameEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ PACMAN/PacmanMap.java
@@ -0,0 +1,9 @@
+
+public class PacmanMap extends Map {
+
+	public PacmanMap(int Xsize, int Ysize, Game game) {
+		super(Xsize, Ysize, game);
+		// TODO Auto-generated constructor stub
+	}
+
+}
