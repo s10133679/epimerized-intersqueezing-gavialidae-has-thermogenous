eeee95405d8e3a166985217140b9409d681bb89a
aspiring.arsonist <aspiring.arsonist@gmail.com>
2011-02-28 09:44:29 +0000

    stupid svn

---
+++ src/Item.java
@@ -0,0 +1,11 @@
+
+public abstract class Item extends Mappable {
+
+	public Item(int x, int y) {
+		super(x, y);
+		// TODO Auto-generated constructor stub
+	}
+	public abstract void action(Object obj);
+	public abstract void onEvent(GameEvent e);
+	
+}
