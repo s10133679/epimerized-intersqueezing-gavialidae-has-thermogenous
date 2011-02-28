659381df438b7d68f223cd3647ac804c19539dc1
aspiring.arsonist@gmail.com <aspiring.arsonist@gmail.com@38fbf40a-4406-2c4f-7d2f-54aa74e2a72e>
2011-02-28 16:53:49 +0000

    Pacman class

---
+++ PACMAN/Pacman.java
@@ -0,0 +1,21 @@
+
+public class Pacman extends Player {
+
+	public Pacman(int x, int y) {
+		super(x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void spawn() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onEvent(GameEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
