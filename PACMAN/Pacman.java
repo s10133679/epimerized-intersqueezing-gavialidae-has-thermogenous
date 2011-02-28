49e16702409557703845961b16a4ef7444a291d9
aspiring.arsonist <aspiring.arsonist@gmail.com>
2011-02-28 21:52:26 +0000

    Added beastmode capability

--- PACMAN/Pacman.java
+++ PACMAN/Pacman.java
@@ -1,6 +1,6 @@
 
 public class Pacman extends Player implements GameListener{
-
+	private PacmanState state;
 	public Pacman(int x, int y) {
 		super(x, y);
 		// TODO Auto-generated constructor stub
@@ -8,7 +8,8 @@ public class Pacman extends Player implements GameListener{
 	
 	public Pacman(int x, int y, Direction direction, int numOfLives) {
 		super(x,y,direction,numOfLives);
-		setImage("PACMAN/pacmanimg.png");		
+		setImage("PACMAN/pacmanimg.png");
+		state = PacmanState.NORMAL;
 	}
 
 	@Override
@@ -20,6 +21,20 @@ public class Pacman extends Player implements GameListener{
 	public void onEvent(GameEvent e) {
 		// TODO Auto-generated method stub
 
+	}
+	/**
+	 * Allows pacman's state to be changed.
+	 * @param state Pacman's new state
+	 */
+	public void setState(PacmanState state) {
+		this.state = state;
+	}
+
+	/**
+	 * @return Returns pacmans current state
+	 */
+	public PacmanState getState() {
+		return state;
 	}
 
 }
