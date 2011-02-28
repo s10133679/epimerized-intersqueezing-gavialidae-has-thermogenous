585b8006b8040bde8744e24d6dbd90de4222f4d8
aspiring.arsonist@gmail.com <aspiring.arsonist@gmail.com@38fbf40a-4406-2c4f-7d2f-54aa74e2a72e>
2011-02-28 15:45:23 +0000

    added proper commenting

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -19,8 +19,12 @@ public abstract class GamePanel extends JPanel{
 	private Color mazeColor;
 	private Color bgColor;
 	private Dimension d;
-	private final int defaultTileSize = 20; //in pixels
+	private final int defaultTileSize = 19; //in pixels
 	
+	/**
+	 * Creates a game panel that can be added to a JFrame for a game
+	 * @param g instance of game.
+	 */
 	public GamePanel(Game g){
 		this.game = g;
 		addKeyListener(new KeyPressHandler());
@@ -35,28 +39,55 @@ public abstract class GamePanel extends JPanel{
 
 
 	}
-	
+	/**
+	 * (non-Javadoc)
+	 * @see javax.swing.JComponent#addNotify()
+	 */
 	public void addNotify() {
         super.addNotify();
         GameInit();
     }
 
+	/**
+	 * Performs Game initializations.
+	 */
 	public abstract void GameInit();
 
+	/**
+	 * Changes current background colour
+	 * @param color New color for background
+	 */
 	public void setBgColor( Color color){
 		this.bgColor = color;
 	}
 	
+	/**
+	 * Changes current wall colour
+	 * @param color New color for walls in the map
+	 */
 	public void setMazeColor( Color color){
 		this.mazeColor = color;
 	}
 	
+	/**
+	 * Provides a simple draw method
+	 * @param image Image to be drawn
+	 * @param y Y co-ordinate on canvas of whare Image should be drawn
+	 * @param x X co-ordinate on canvas of where Image should be drawn
+	 * @param g2d Instance of current Graphics on which things are to be drawn
+	 */
 	public void DrawImage(Graphics2D g2d, int x, int y, Image image) {
         g2d.drawImage(image, x, y, this);
     }
 
+	/**
+	 * Starts a new game
+	 */
 	public abstract void newGame();
-	
+	/**
+	 * (non-Javadoc)
+	 * @see javax.swing.JComponent#paint(java.awt.Graphics)
+	 */
 	public void paint(Graphics g){
 		super.paint(g);
 
@@ -72,7 +103,10 @@ public abstract class GamePanel extends JPanel{
 
 		
 	}
-	
+	/**
+	 * Draws everything loaded onto current map that is contained in the current Game
+	 * @param g2d Instance of Graphics on which images are to be drawn
+	 */
 	private void DrawMap(Graphics2D g2d) {
 		for(int x = 0; x < this.game.getMap().getX(); x++){
 			for(int y = 0; y < this.game.getMap().getY(); y++){
@@ -92,16 +126,22 @@ public abstract class GamePanel extends JPanel{
 			}
 		}
 	}
-
+	/**
+	 *	Handy adapter to listen for key presses 
+	 */
 	class KeyPressHandler extends KeyAdapter{
 		public void keyPressed(KeyEvent e){
-			int key = e.getKeyCode();
-			switch(key){
-			//Add cases for different key presses here
-			}
+			onKeyPress(e.getKeyCode());
 		}
 	}
-	
+	/**
+	 * Abstract method to allow for custom key bindings for different games
+	 * @param keycode integer representation of current key being pressed
+	 */
+	public abstract void onKeyPress(int keycode);
+	/**
+	 * Simply redraws everything on any event
+	 */
 	public void actionPerformed(ActionEvent e) {
 	        repaint();  
 	}
