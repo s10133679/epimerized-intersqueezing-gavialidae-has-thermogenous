61c6dd27f49edfdd1191490a02d5e6ccc0e05600
aspiring.arsonist@gmail.com <aspiring.arsonist@gmail.com@38fbf40a-4406-2c4f-7d2f-54aa74e2a72e>
2011-02-28 15:52:50 +0000

    Added proper commenting

--- src/Player.java
+++ src/Player.java
@@ -2,7 +2,7 @@
 public abstract class Player extends Mappable {
 	private Direction direction;
 	private int numOflives;
-	/*
+	/**
 	 * General purpose constructor for Player. Sets direction to Direction.LEFT and numOfLives to 1.
 	 * @param x Desired X position of Player
 	 * @param y Desired Y position of Player
@@ -12,7 +12,7 @@ public abstract class Player extends Mappable {
 		this.direction = Direction.LEFT;
 		this.numOflives = 1;
 	}
-	/*
+	/**
 	 * A more specific constructor for Player. Allows direction and numOfLives to be set to desired values.
 	 * @param x Desired X position of Player
 	 * @param y Desired Y position of Player
@@ -26,14 +26,14 @@ public abstract class Player extends Mappable {
 		
 	}
 	
-	/*
+	/**
 	 * @return Current direction of Player
 	 */
 	public Direction getDirection() {
 		return direction;
 	}
 	
-	/*
+	/**
 	 * Change the current direction of Player
 	 * @param direction Either UP, DOWN, LEFT or RIGHT
 	 */
@@ -41,21 +41,21 @@ public abstract class Player extends Mappable {
 		this.direction = direction;
 	}
 	
-	/*
+	/**
 	 * @return Number of lives of the Player
 	 */
 	public int getNumOflives() {
 		return numOflives;
 	}
 	
-	/*
+	/**
 	 * Change the current number of lives of the Player
 	 * @param numOflives number of lives desired.
 	 */
 	public void setNumOflives(int numOflives) {
 		this.numOflives = numOflives;
 	}
-	/*
+	/**
 	 * Moves the Player in its current direction.
 	 * @param Map current map the Player is on
 	 * @return true if the move was successful, false otherwise.
@@ -97,7 +97,7 @@ public abstract class Player extends Mappable {
 
 	public abstract void spawn();
 	
-	/*
+	/**
 	 * Decrements current lives by one.
 	 */
 	public void die(){
