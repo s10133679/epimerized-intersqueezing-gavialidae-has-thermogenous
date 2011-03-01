750e7033f4cc7c5ea1778fe8d66185798ac1a672
aspiring.arsonist@gmail.com <aspiring.arsonist@gmail.com@38fbf40a-4406-2c4f-7d2f-54aa74e2a72e>
2011-03-01 05:16:11 +0000

    fiexed

---
+++ MOUSELAND/Hero.java
@@ -0,0 +1,27 @@
+
+public class Hero extends Player {
+
+	public Hero(int x, int y, Direction direction, int numOflives) {
+		super(x, y, direction, numOflives);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void spawn(Map map) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void die(Map map) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onEvent(GameEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
