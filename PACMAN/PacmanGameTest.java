ee0a4548275b83029fbac5f871a56fe388cc70a7
Argonanth@gmail.com <Argonanth@gmail.com@38fbf40a-4406-2c4f-7d2f-54aa74e2a72e>
2011-03-01 20:24:07 +0000

    added classes for the remaining unit tests

---
+++ PACMAN/PacmanGameTest.java
@@ -0,0 +1,31 @@
+import junit.framework.TestCase;
+
+
+public class PacmanGameTest extends TestCase {
+
+	public PacmanGameTest(String arg0) {
+		super(arg0);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ PACMAN/PacmanMapTest.java
@@ -0,0 +1,18 @@
+import junit.framework.TestCase;
+
+
+public class PacmanMapTest extends TestCase {
+
+	public PacmanMapTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
