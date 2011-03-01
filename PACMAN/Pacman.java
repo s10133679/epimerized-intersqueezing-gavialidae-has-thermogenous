8fa2a694810e942ba6d91d099ce2e56dd8e8b982
Argonanth@gmail.com <Argonanth@gmail.com@38fbf40a-4406-2c4f-7d2f-54aa74e2a72e>
2011-03-01 00:54:31 +0000

    just updating the attempt to eat/die to ghosts... not working yet though

--- PACMAN/Pacman.java
+++ PACMAN/Pacman.java
@@ -21,36 +21,31 @@ public class Pacman extends Player implements GameListener{
 
 	@Override
 	public void onEvent(GameEvent e) {
+		
+		//MOVEMENT
 		if(e.getSource().equals("movement") && e.getGameValue() instanceof PacmanGame) { //if movement has occured
 			PacmanGame tempGame = (PacmanGame)e.getGameValue(); //create a temp variable of the game
 			
 			ArrayList<Mappable> mappables = tempGame.getMap().getMappable(getX(),getY());
+			state = PacmanState.BEASTMODE;
 			
-			for(int i=0; i<mappables.size(); i++) { //go through array to check if pacman is on a LittlePillItem
+			for(int i=mappables.size()-1; i>0; i--) { //go through array to check if pacman is on a LittlePillItem
 				if(mappables.get(i) instanceof LittlePillItem) {
 					tempGame.getMap().removeMappable(getX(),getY(),i);
 					tempGame.setScore(tempGame.getScore()+1); //increment score by 1
 					tempGame.setPillsLeft(tempGame.getPillsLeft()-1); //decrement pillsLeft by 1
 				}
 			}
-			for(int i=0; i<mappables.size(); i++) { //go through array to check if pacman is on a Ghost
+			
+			//not working
+			for(int i=mappables.size()-1; i>0; i--) { //go through array to check if pacman is on a Ghost
 				if(mappables.get(i) instanceof Ghost) {
-					if(state == PacmanState.NORMAL) {
-						if(getNumOflives() == 0) {
-							tempGame.end();
-						}
-						else { //has lives left
-							
-						}
-						die(); //decrement lives left
-						tempGame.getMap().removeMappable(getX(),getY(),i); //remove pacman
-						setX(9); 
-						setY(9);
-						spawn(tempGame.getMap()); //respawn pacman
-						break; //break in case you were on 2 ghosts... dont want to die 2 times (errors)
+					if(state == PacmanState.NORMAL) { //NORMAL
+						
+						break;
 					}
 					else { //BEASTMODE
-						
+						tempGame.getMap().removeMappable(getX(),getY(),i);
 					}
 				}
 			}
