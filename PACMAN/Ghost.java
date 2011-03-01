f367f8539b69998621fd2f98ac460dd88b0f0453
MacDougall.Colin <MacDougall.Colin@38fbf40a-4406-2c4f-7d2f-54aa74e2a72e>
2011-03-01 02:12:15 +0000

    Completely reworked ghost movement. They're now vicious beasts. Good Luck.

--- PACMAN/Ghost.java
+++ PACMAN/Ghost.java
@@ -26,141 +26,134 @@ public class Ghost extends Player implements GameListener {
 		// TODO Auto-generated method stub
 
 	}
-	/**This function receives the x and y coordinates and the current state of Pacman and 
-	 * determines where the ghosts should move. Basic movement logic is shown below:
-	 * 
-	 * -----MOVE GHOSTS-----
-	 * Compare X position of Ghost in Map to Pacman's X to find the delta X distance
-	 * Compare Y position of Ghost in Map to Pacman's Y to find the delta Y distance
-	 * If X is less distance than Y, if pacman is in beast mode
-	 * 		run ghosts X away
-	 * 		if there is a wall there, run Y away
-	 * 		if there is a wall there, run Y towards
-	 * 		if there is a wall there, run X towards
-	 * 		if there is a wall there
-	 * Similar process for if Y is less distance than X. 
-	 * If Pacman is not in beast mode, replace away with towards for each case.
-	 *
+	/**
+	 * This function dictates the movement of the ghosts.
+	 * When pacman is normal mode, the ghosts viciously chase you down, deciding every move which square
+	 * will lead to the shortest distance to you, and take that path.
+	 * If pacman is BEAST MODE, the ghosts will run away from you, taking the furthest path away from
+	 * you by deciding this each movement, for every possible square
+	 * @param X
+	 * @param Y
+	 * @param mode
+	 * @param map
 	 */
 	public void moveGhosts(int X, int Y, PacmanState mode, Map map){
-		int distance_x = X - getX();
-		int distance_y = Y - getY();
-		if(mode == PacmanState.BEASTMODE){
-			if((distance_x * distance_x) <= (distance_y * distance_y)){
-					if(distance_x > 0){
-						this.setDirection(Direction.LEFT);
-					}else{
-						this.setDirection(Direction.RIGHT);
-					}
-					if(!this.updateLocation(map)){
-						if(distance_y > 0){
-							this.setDirection(Direction.UP);
-						}else{
-							this.setDirection(Direction.DOWN);
-						}
-						if(!this.updateLocation(map)){
-							if(distance_y > 0){
-								this.setDirection(Direction.DOWN);
-							}else{
-								this.setDirection(Direction.UP);
-							}
-							if(!this.updateLocation(map)){
-								if(distance_x > 0){
-									this.setDirection(Direction.RIGHT);
-								}else{
-									this.setDirection(Direction.LEFT);
-								}
-									this.updateLocation(map);
-							}
+		//for the purpose of this method, 0 is up, 1 is right, 2 is down, 3 is left
+		double uDist = 0;
+		double rDist = 0;
+		double dDist = 0;
+		double lDist = 0;
+
+		//find the distance the ghost is from pacman for each possible move
+		for (int i = 0; i < 4; i++){
+			switch(i){
+			case 0://UP
+				if ((map.isWall(this.getX(), this.getY()-1)) == false){
+					//not a wall, you can move up
+					uDist = Math.sqrt(Math.pow(Math.abs(Y-(this.getY()-1)),2) + Math.pow(Math.abs(X-this.getX()),2));
+					}else{//there is a wall
+						if (mode == PacmanState.BEASTMODE){
+							uDist = 0; //set to small number so this will not be chosen as a possible path
+						}else{ //NORMAL
+							uDist = 1000; //set to large number so when normal, path won't be chosen
 						}
 					}
-			}else{ //X >= Y
-				if(distance_y > 0){
-					this.setDirection(Direction.UP);
-				}else{
-					this.setDirection(Direction.DOWN);
-				}
-				if(!this.updateLocation(map)){
-					if(distance_x > 0){
-						this.setDirection(Direction.LEFT);
-					}else{
-						this.setDirection(Direction.RIGHT);
-					}
-					if(!this.updateLocation(map)){
-						if(distance_x > 0){
-							this.setDirection(Direction.RIGHT);
-						}else{
-							this.setDirection(Direction.LEFT);
-						}
-						if(!this.updateLocation(map)){
-							if(distance_y > 0){
-								this.setDirection(Direction.DOWN);
-							}else{
-								this.setDirection(Direction.UP);
-							}
-								this.updateLocation(map);
-						}
+				break;
+			case 1://RIGHT
+				if ((map.isWall(this.getX()+1, this.getY())) == false){
+					//not a wall, you can move right
+					rDist = Math.sqrt(Math.pow(Math.abs(Y-this.getY()),2) + Math.pow(Math.abs(X-(this.getX()+1)),2));
+				}else{//there is a wall
+					if (mode == PacmanState.BEASTMODE){
+						rDist = 0; //set to small number so this will not be chosen as a possible path
+					}else{ //NORMAL
+						rDist = 1000; //set to large number so when normal, path won't be chosen
 					}
 				}
-			}
-		}else{ //normal mode
-			if((distance_x * distance_x) <= (distance_y * distance_y)){
-				if(distance_x > 0){
-					this.setDirection(Direction.RIGHT);
-				}else{
-					this.setDirection(Direction.LEFT);
-				}
-				if(!this.updateLocation(map)){
-					if(distance_y > 0){
-						this.setDirection(Direction.DOWN);
-					}else{
-						this.setDirection(Direction.UP);
-					}
-					if(!this.updateLocation(map)){
-						if(distance_y > 0){
-							this.setDirection(Direction.UP);
-						}else{
-							this.setDirection(Direction.DOWN);
-						}
-						if(!this.updateLocation(map)){
-							if(distance_x > 0){
-								this.setDirection(Direction.LEFT);
-							}else{
-								this.setDirection(Direction.RIGHT);
-							}
-								this.updateLocation(map);
-						}
+				break;
+			case 2://DOWN
+				if ((map.isWall(this.getX(), this.getY()+1)) == false){
+					//not a wall, you can move down
+					dDist = Math.sqrt(Math.pow(Math.abs(Y-(this.getY()+1)),2) + Math.pow(Math.abs(X-this.getX()),2));
+				}else{//there is a wall
+					if (mode == PacmanState.BEASTMODE){
+						dDist = 0; //set to small number so this will not be chosen as a possible path
+					}else{ //NORMAL
+						dDist = 1000; //set to large number so when normal, path won't be chosen
 					}
 				}
-			}else{ //X >= Y
-				if(distance_y > 0){
-					this.setDirection(Direction.DOWN);
-				}else{
-					this.setDirection(Direction.UP);
-				}
-				if(!this.updateLocation(map)){
-					if(distance_x > 0){
-						this.setDirection(Direction.RIGHT);
-					}else{
-						this.setDirection(Direction.LEFT);
-					}
-					if(!this.updateLocation(map)){
-						if(distance_x > 0){
-							this.setDirection(Direction.LEFT);
-						}else{
-							this.setDirection(Direction.RIGHT);
-						}
-						if(!this.updateLocation(map)){
-							if(distance_y > 0){
-								this.setDirection(Direction.UP);
-							}else{
-								this.setDirection(Direction.DOWN);
-							}
-								this.updateLocation(map);
-						}
+				break;
+			case 3://LEFT
+				if ((map.isWall(this.getX()-1, this.getY())) == false){
+					//not a wall, you can move left
+					lDist = Math.sqrt(Math.pow(Math.abs(Y-this.getY()),2) + Math.pow(Math.abs(X-(this.getX()-1)),2));
+				}else{//there is a wall
+					if (mode == PacmanState.BEASTMODE){
+						lDist = 0; //set to small number so this will not be chosen as a possible path
+					}else{ //NORMAL
+						lDist = 1000; //set to large number so when normal, path won't be chosen
 					}
 				}
+				break;
 			}
 		}
+		// The following switch statement determines the direction you were going, and makes the opposite
+		// direction less desirable, since ghosts should not spontaneously change direction
+		// The desirability of a direction is based on the state of pacman, since we use large or small
+		// numbers to make a direction more or less desirable based on whether pacman is chasing them or
+		// running away from them
+		switch(this.getDirection()){
+		case UP:					
+			if (mode == PacmanState.BEASTMODE){
+				dDist = 1; 
+			}else{ //NORMAL
+				dDist = 990; }			 
+			break;			 
+		case RIGHT:
+			if (mode == PacmanState.BEASTMODE){
+				lDist = 1;
+			}else{ //NORMAL
+				lDist = 990;}
+			break;
+		case DOWN:
+			if (mode == PacmanState.BEASTMODE){
+				uDist = 1;
+			}else{ //NORMAL
+				uDist = 990;}
+			break;
+		case LEFT:
+			if (mode == PacmanState.BEASTMODE){
+				rDist = 1;
+			}else{ //NORMAL
+				rDist = 990;}
+			break;
+		}
+		// By now, the opposite direction the ghost was travelling is less desirable than other paths, but
+		// paths that would lead to walls are the least desirable
+		if(mode == PacmanState.BEASTMODE){//running away
+			//go for the farthest away path
+			if(uDist > rDist && uDist > dDist && uDist > lDist ){
+				this.setDirection(Direction.UP);
+			}else if(rDist > uDist && rDist > dDist && rDist > lDist){
+				this.setDirection(Direction.RIGHT);
+			}else if(dDist > uDist && dDist > rDist && dDist > lDist){
+				this.setDirection(Direction.DOWN);
+			}else if(lDist > uDist && lDist > rDist && lDist > dDist){
+				this.setDirection(Direction.LEFT);
+			}
+		}else{//chasing
+			//decide which distance is the shortest to chase
+			if(uDist < rDist && uDist < dDist && uDist < lDist ){
+				this.setDirection(Direction.UP);
+			}else if(rDist < uDist && rDist < dDist && rDist < lDist){
+				this.setDirection(Direction.RIGHT);
+			}else if(dDist < uDist && dDist < rDist && dDist < lDist){
+				this.setDirection(Direction.DOWN);
+			}else if(lDist < uDist && lDist < rDist && lDist < dDist){
+				this.setDirection(Direction.LEFT);
+			}
+		}
+		this.updateLocation(map); //actually move the ghosts
+	return;
 	}
 }
