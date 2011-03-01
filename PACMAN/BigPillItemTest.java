db98d7292739e7b6e428af922096d2d3f5af744f
Argonanth <Argonanth@gmail.com>
2011-03-01 18:46:05 +0000

    added comments saying which methds i wrote

--- PACMAN/BigPillItemTest.java
+++ PACMAN/BigPillItemTest.java
@@ -1,8 +1,14 @@
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+
 import junit.framework.TestCase;
 
 
 public class BigPillItemTest extends TestCase {
 	BigPillItem testBigPillNorm, testBigPillZero, testBigPillNeg, testBigPillHuge;
+	Image testImage;
 
 	public BigPillItemTest(String arg0) {
 		super(arg0);
@@ -14,6 +20,8 @@ public class BigPillItemTest extends TestCase {
 		testBigPillNeg = new BigPillItem(-5,-10);
 		testBigPillHuge = new BigPillItem(5000,10000);
 		
+		Image testImage = new ImageIcon("PACMAN/smallpill.png").getImage();
+		
 	}
 
 	protected void tearDown() throws Exception {
@@ -21,7 +29,7 @@ public class BigPillItemTest extends TestCase {
 	}
 
 	
-	
+	//MAPPABLE TESTS
 	public void testGetSetX() {
 		//TEST GET INITIAL VALUES
 		assertEquals(testBigPillNorm.getX(), 5);
@@ -50,9 +58,29 @@ public class BigPillItemTest extends TestCase {
 		assertEquals(testBigPillNorm.getY(), 8);
 	}
 	
-	
-	
-	
+	public void testToString() {
+		assertEquals(testBigPillNorm.toString(),"5,10");
+		assertEquals(testBigPillZero.toString(),"0,0");
+		assertEquals(testBigPillNeg.toString(),"-5,-10");
+		assertEquals(testBigPillHuge.toString(),"5000,10000");
+	}
+		
+	public void testGetSetImageImage() {
+		//DEFAULT IMAGE IS "PACMAN/bigpill.png"
+		assertEquals(testBigPillNorm.getImage(), new ImageIcon("PACMAN/bigpill.png").getImage());
+		
+		//SET NEW IMAGE
+		testBigPillNorm.setImage(testImage);
+		assertEquals(testBigPillNorm.getImage(), testImage);
+	}
+	public void testGetSetImageString() {
+		//DEFAULT IMAGE IS "PACMAN/bigpill.png"
+		assertEquals(testBigPillNorm.getImage(), new ImageIcon("PACMAN/bigpill.png").getImage());
+		
+		//SET NEW IMAGE ("PACMAN/littlepill.png" because it exists)
+		testBigPillNorm.setImage("PACMAN/smallpill.png");
+		assertEquals(testBigPillNorm.getImage(), testImage);
+	}
 	
 	
 	
