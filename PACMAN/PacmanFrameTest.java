b016cd0326e91c54502b6082872f6554e34d1012
Argonanth <Argonanth@gmail.com>
2011-03-01 20:24:07 +0000

    added classes for the remaining unit tests

---
+++ PACMAN/PacmanFrameTest.java
@@ -0,0 +1,18 @@
+import junit.framework.TestCase;
+
+
+public class PacmanFrameTest extends TestCase {
+
+	public PacmanFrameTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
