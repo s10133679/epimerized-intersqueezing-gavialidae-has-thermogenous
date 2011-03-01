0ec906b305bcc29f9b3f5dd03aa0362a92db16c8
josh.delrio@gmail.com <josh.delrio@gmail.com@38fbf40a-4406-2c4f-7d2f-54aa74e2a72e>
2011-03-01 05:37:30 +0000

    MouselandMap done

--- MOUSELAND/MouselandMap.java
+++ MOUSELAND/MouselandMap.java
@@ -1,9 +1,13 @@
 
 public class MouselandMap extends Map {
 
-	public MouselandMap(String fileName) {
-		super(fileName);
+	public MouselandMap(int Xsize, int Ysize) {
+		super(Xsize, Ysize);
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	public MouselandMap(String filename) {
+		super(filename);
+		// TODO Auto-generated constructor stub
+	}
 }
